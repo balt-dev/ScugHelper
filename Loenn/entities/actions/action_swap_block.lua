0487e2d5ecf8fb7f55bd898580447111a7cf603b
balt-dev <heptor42@gmail.com>
2026-05-07 21:48:31 -0500

    swap

---
+++ Loenn/entities/actions/action_swap_block.lua
@@ -0,0 +1,150 @@
+local scughelper = require("mods").requireFromPlugin("libraries.scughelper")
+local drawableNinePatch = require("structs.drawable_nine_patch")
+local utils = require("utils")
+
+local swapBlock = {}
+
+local frameNinePatchOptions = {
+    mode = "fill",
+    borderMode = "repeat"
+}
+
+local frameNodeNinePatchOptions = {
+    mode = "fill",
+    borderMode = "repeat",
+    color =  {1.0, 1.0, 1.0, 0.7}
+}
+
+local trailNinePatchOptions = {
+    mode = "fill",
+    borderMode = "repeat",
+    useRealSize = true
+}
+
+local pathNinePatchOptions = {
+    mode = "fill",
+    fillMode = "repeat",
+    border = 0
+}
+
+local pathDepth = 8999
+local trailDepth = 8999
+local blockDepth = -9999
+
+swapBlock.name = "ScugHelper/ActionSwapBlock"
+swapBlock.nodeLimits = {1, 1}
+swapBlock.fieldInformation = {
+    Groups = {fieldType = "list", elementOptions = scughelper.actionGroups, elementDefault = ""}
+}
+local placements = {
+    { "normal", "#PlayerDash" },
+    { "jump", "#PlayerJump" },
+    { "grab", "#PlayerGrab" },
+    { "action", "" },
+}
+swapBlock.placements = {}
+for _, pair in ipairs(placements) do
+    table.insert(swapBlock.placements,
+    {
+        name = pair[1],
+        data = {
+            width = 16,
+            height = 16,
+            Particles = true,
+            HidePath = false,
+            HideBackground = false,
+            HideMiddle = false,
+            Toggle = pair.toggle or false,
+            Groups = pair[2],
+            PathSprite = "objects/swapblock/path",
+            ReturnTime = 0.8,
+            InactiveBlockSprite = "objects/swapblock/block",
+            ActiveBlockSprite = "objects/swapblock/blockRed",
+            BackgroundSprite = "objects/swapblock/target",
+            InactiveMiddleSprite = "swapBlockLight",
+            ActiveMiddleSprite = "swapBlockLightRed",
+            MovementSpeed = 360,
+            ReturnSpeedMultiplier = 0.4,
+        }
+    }
+ )
+end
+
+swapBlock.warnBelowSize = {16, 16}
+
+local function addBlockSprites(sprites, entity, position, frameTexture, isNode)
+    local x, y = position.x or 0, position.y or 0
+    local width, height = entity.width or 8, entity.height or 8
+
+    local ninePatchOptions = isNode and frameNodeNinePatchOptions or frameNinePatchOptions
+    local frameNinePatch = drawableNinePatch.fromTexture(frameTexture, ninePatchOptions, x, y, width, height)
+    local frameSprites = frameNinePatch:getDrawableSprite()
+
+    for _, sprite in ipairs(frameSprites) do
+        sprite.depth = blockDepth
+
+        table.insert(sprites, sprite)
+    end
+end
+
+local function addTrailSprites(sprites, entity, trailTexture, path)
+    local nodes = entity.nodes or {}
+    local x, y = entity.x or 0, entity.y or 0
+    local nodeX, nodeY = nodes[1].x or x, nodes[1].y or y
+    local width, height = entity.width or 8, entity.height or 8
+    local drawWidth, drawHeight = math.abs(x - nodeX) + width, math.abs(y - nodeY) + height
+
+    x, y = math.min(x, nodeX), math.min(y, nodeY)
+
+    if path then
+        local pathDirection = x == nodeX and "V" or "H"
+        local pathTexture = string.format(entity.PathSprite .. "%s", pathDirection)
+        local pathNinePatch = drawableNinePatch.fromTexture(pathTexture, pathNinePatchOptions, x, y, drawWidth, drawHeight)
+        local pathSprites = pathNinePatch:getDrawableSprite()
+
+        for _, sprite in ipairs(pathSprites) do
+            sprite.depth = pathDepth
+
+            table.insert(sprites, sprite)
+        end
+    end
+
+    if (not entity.HideBackground) then
+        local frameNinePatch = drawableNinePatch.fromTexture(trailTexture, trailNinePatchOptions, x, y, drawWidth, drawHeight)
+        local frameSprites = frameNinePatch:getDrawableSprite()
+
+        for _, sprite in ipairs(frameSprites) do
+            sprite.depth = trailDepth
+
+            table.insert(sprites, sprite)
+        end
+    end
+end
+
+function swapBlock.sprite(room, entity)
+    local sprites = {}
+
+    addTrailSprites(sprites, entity, entity.BackgroundSprite, not entity.HidePath)
+    addBlockSprites(sprites, entity, entity, entity.InactiveBlockSprite)
+
+    return sprites
+end
+
+function swapBlock.nodeSprite(room, entity, node)
+    local sprites = {}
+
+    addBlockSprites(sprites, entity, entity, entity.InactiveBlockSprite, true)
+
+    return sprites
+end
+
+function swapBlock.selection(room, entity)
+    local nodes = entity.nodes or {}
+    local x, y = entity.x or 0, entity.y or 0
+    local nodeX, nodeY = nodes[1].x or x, nodes[1].y or y
+    local width, height = entity.width or 8, entity.height or 8
+
+    return utils.rectangle(x, y, width, height), {utils.rectangle(nodeX, nodeY, width, height)}
+end
+
+return swapBlock
---
+++ Source/Entities/Actions/ActionSwapBlock.cs
@@ -0,0 +1,173 @@
+using Microsoft.Xna.Framework;
+using Monocle;
+using Celeste.Mod.Entities;
+using System;
+using MonoMod;
+namespace Celeste.Mod.ScugHelper.Entities.Actions;
+
+[TrackedAs(typeof(SwapBlock))]
+[CustomEntity("ScugHelper/ActionSwapBlock")]
+public class ActionSwapBlock : SwapBlock
+{
+
+    internal class CustomPathRenderer : PathRenderer
+    {
+        public CustomPathRenderer(ActionSwapBlock block)
+            : base(block) => pathTexture = GFX.Game[block.PathSprite + ((block.start.X == block.end.X) ? "V" : "H")];
+    }
+
+    readonly string[] Groups;
+    readonly bool Toggle;
+    readonly bool Particles;
+    readonly string PathSprite;
+    readonly new float ReturnTime;
+
+    private static readonly MTexture transparent = new(VirtualContent.CreateTexture("transparent", 24, 24, Color.Transparent));
+
+    public ActionSwapBlock(EntityData data, Vector2 offset)
+        : base(
+            data.Position + offset, data.Width, data.Height,
+            data.FirstNodeNullable(offset) ?? throw new Exception("Swap block must have end node."),
+            data.Bool("HidePath") ? Themes.Moon : Themes.Normal
+        )
+    {
+        Particles = data.Bool("Particles");
+        Toggle = data.Bool("Toggle");
+        Groups = IAction.GetGroups(data.String("Groups", "#PlayerDash"));
+        Add(new ActionListener(Groups, OnAlert));
+
+        Components.RemoveAll<DashListener>();
+        Components.RemoveAll<Sprite>();
+
+        PathSprite = data.String("PathSprite", "objects/swapblock/path");
+        ReturnTime = data.Float("ReturnTime", 0.8f);
+
+        var inactive = GFX.Game[data.String("InactiveBlockSprite", "objects/swapblock/block")];
+        var active = GFX.Game[data.String("ActiveBlockSprite", "objects/swapblock/blockRed")];
+        var background =
+            data.Bool("HideBackground")
+            ? transparent
+            : GFX.Game[data.String("BackgroundSprite", "objects/swapblock/target")];
+
+        nineSliceGreen = new MTexture[3, 3];
+        nineSliceRed = new MTexture[3, 3];
+        nineSliceTarget = new MTexture[3, 3];
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                nineSliceGreen[i, j] = inactive.GetSubtexture(new Rectangle(i * 8, j * 8, 8, 8));
+                nineSliceRed[i, j] = active.GetSubtexture(new Rectangle(i * 8, j * 8, 8, 8));
+                nineSliceTarget[i, j] = background.GetSubtexture(new Rectangle(i * 8, j * 8, 8, 8));
+            }
+        }
+
+        middleGreen = null;
+        middleRed = null;
+        if (!data.Bool("HideMiddle")) {
+            Add(middleGreen = GFX.SpriteBank.Create(data.String("InactiveMiddleSprite", "swapBlockLight")));
+            Add(middleRed = GFX.SpriteBank.Create(data.String("ActiveMiddleSprite", "swapBlockLightRed")));
+        }
+
+        maxForwardSpeed = data.Float("MovementSpeed", 360) / Vector2.Distance(start, end);
+        maxBackwardSpeed = maxForwardSpeed * data.Float("ReturnSpeedMultiplier", 0.4f);
+    }
+
+    private void OnAlert(Level level)
+    {
+        returnTimer = ReturnTime;
+        Swapping = Toggle || lerp < 1f;
+        target = Toggle ? 1 - target : 1;
+        burst = (Scene as Level).Displacement.AddBurst(Center, 0.2f, 0f, 16f);
+        var absLerp = target == 0 ? 1 - lerp : lerp;
+        speed = absLerp >= 0.2 ? maxForwardSpeed : MathHelper.Lerp(maxForwardSpeed * 0.333f, maxForwardSpeed, absLerp / 0.2f);
+
+        Audio.Stop(returnSfx);
+        Audio.Stop(moveSfx);
+        if (!Swapping)
+            Audio.Play("event:/game/05_mirror_temple/swapblock_move_end", Center);
+        else
+            moveSfx = Audio.Play("event:/game/05_mirror_temple/swapblock_move", Center);
+    }
+
+    public override void Awake(Scene scene) {
+        base.Awake(scene);
+        path.RemoveSelf();
+        scene.Add(path = new CustomPathRenderer(this));
+    }
+
+    [MonoModLinkTo("Celeste.Solid", "System.Void Update")]
+    private void SolidUpdate() { }
+
+    public override void Update()
+    {
+        SolidUpdate();
+
+        if (returnTimer > 0f && !Toggle)
+        {
+            returnTimer -= Engine.DeltaTime;
+            if (returnTimer <= 0f)
+            {
+                target = 1 - target;
+                speed = 0f;
+                Audio.Stop(returnSfx);
+                returnSfx = Audio.Play("event:/game/05_mirror_temple/swapblock_return", Center);
+            }
+        }
+
+        burst?.Position = Center;
+
+        redAlpha = Calc.Approach(redAlpha, (target != 1) ? 1 : 0, Engine.DeltaTime * 32f);
+        if (target == lerp && (Toggle || lerp == 0))
+        {
+            middleRed?.SetAnimationFrame(0);
+            middleGreen?.SetAnimationFrame(0);
+        }
+
+        if (Toggle || target == 1)
+            speed = Calc.Approach(speed, maxForwardSpeed, maxForwardSpeed / 0.2f * Engine.DeltaTime);
+        else
+            speed = Calc.Approach(speed, maxBackwardSpeed, maxBackwardSpeed / 1.5f * Engine.DeltaTime);
+        float num = lerp;
+        lerp = Calc.Approach(lerp, target, speed * Engine.DeltaTime);
+        if (lerp != num)
+        {
+            Vector2 liftSpeed = (end - start) * speed;
+
+            if (lerp < num)
+                liftSpeed *= -1f;
+
+            if (Particles && (Toggle || target == 1) && Scene.OnInterval(0.02f))
+                MoveParticles(end - start);
+
+            MoveTo(Vector2.Lerp(start, end, lerp), liftSpeed);
+            Audio.Position(moveSfx, Center);
+            Audio.Position(returnSfx, Center);
+
+
+            if (Toggle)
+            {
+                if (lerp <= 0 || lerp >= 1)
+                {
+                    Audio.SetParameter(returnSfx, "end", 1f);
+                    Audio.Play("event:/game/05_mirror_temple/swapblock_return_end", Center);
+                }
+            }
+            else
+            {
+                if (lerp <= 0 && target == 0)
+                {
+                    Audio.SetParameter(returnSfx, "end", 1f);
+                    Audio.Play("event:/game/05_mirror_temple/swapblock_return_end", Center);
+                }
+                else if (lerp >= 1 && target == 1)
+                    Audio.Play("event:/game/05_mirror_temple/swapblock_move_end", Center);
+            }
+        }
+
+        if (Swapping && (lerp >= 1 || lerp <= 0))
+            Swapping = false;
+
+        StopPlayerRunIntoAnimation = lerp <= 0f || lerp >= 1f;
+    }
+}
