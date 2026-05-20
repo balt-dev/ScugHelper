2c332f72649b2dd5a60fddee400e48ca6feeed5a
balt-dev <heptor42@gmail.com>
2026-05-19 19:53:40 -0500

    oops

--- Source/Entities/CustomInvisibleBarrier.cs
+++ Source/Entities/CustomInvisibleBarrier.cs
@@ -19,6 +19,7 @@ public class CustomInvisibleBarrier : InvisibleBarrier
 
     public CustomInvisibleBarrier(EntityData data, Vector2 offset) : base(data, offset)
     {
+        Player _;
         SurfaceSoundIndex = data.Int("SurfaceSoundIndex", 33);
         Flag = data.String("Flag");
         FlagState = data.Bool("FlagState");
