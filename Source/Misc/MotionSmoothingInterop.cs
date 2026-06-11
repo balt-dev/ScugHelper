7f1fe5ae881e30d82b76dd20a0f7fd2ec100694d
balt-dev <heptor42@gmail.com>
2026-06-10 19:25:28 -0500

    Fix interop

--- Source/Misc/MotionSmoothingInterop.cs
+++ Source/Misc/MotionSmoothingInterop.cs
@@ -6,19 +6,19 @@ namespace Celeste.Mod.ScugHelper;
 [ModImportName("MotionSmoothing")]
 internal static class MotionSmoothingImportHandler {
     public delegate void DisableInterpolationDelegate(Entity entity);
-    public static DisableInterpolationDelegate? DisableInterpolation = null;
+    public static DisableInterpolationDelegate? DisableObjectSmoothing = null;
 
     public delegate void ReenableInterpolationDelegate(Entity entity);
-    public static DisableInterpolationDelegate? ReenableInterpolation = null;
+    public static DisableInterpolationDelegate? ReenableObjectSmoothing = null;
 }
 
 public static class MotionSmoothingImports {
     public static void DisableInterpolation(this Entity self) =>
-        MotionSmoothingImportHandler.DisableInterpolation?.Invoke(self);
+        MotionSmoothingImportHandler.DisableObjectSmoothing?.Invoke(self);
         
     public static void ReenableInterpolation(this Entity self) =>
-        MotionSmoothingImportHandler.ReenableInterpolation?.Invoke(self);
+        MotionSmoothingImportHandler.ReenableObjectSmoothing?.Invoke(self);
         
     public static bool IsLoaded
-        => MotionSmoothingImportHandler.DisableInterpolation is not null && MotionSmoothingImportHandler.ReenableInterpolation is not null;
+        => MotionSmoothingImportHandler.DisableObjectSmoothing is not null && MotionSmoothingImportHandler.ReenableObjectSmoothing is not null;
 }
